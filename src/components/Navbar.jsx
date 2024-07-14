cb8896296710dd2c3719dffe60e21fcc83838292
lomatul <mahzabin@iut-dhaka.edu>
2024-07-14 16:06:13 +0600

    HomePage Frontend Done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-      <div className="navbar bg-blue-200" >
+      <div className="navbar bg-pink-950" >
   <div className="flex-1">
-    <a className="btn btn-ghost text-xl">ImpactGraph</a>
+    <a className="btn btn-ghost text-2xl text-white">ImpactGraph</a>
   </div>
   <div className="flex-none">
-    <ul className="menu menu-horizontal px-1">
+    <ul className="menu menu-horizontal px-1 text-xl text-white">
       <li><a>Add</a></li>
       <li><a>View Graph</a></li>
       <li><a>Update</a></li>
