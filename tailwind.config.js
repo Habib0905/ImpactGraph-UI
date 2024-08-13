5220b4af653e3979997032c40b16fce3712b8c5f
Lomatul Mahzabin <90206489+lomatul@users.noreply.github.com>
2024-08-13 11:43:28 +0600

    config file added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+
+    extend: {
+
+    keyframes: {
+        "fade-in-left": {
+          "0%": {
+              opacity: 0,
+              transform: "translate3d(-100%, 0, 0)",
+          },
+          "100%": {
+              opacity: 1,
+              transform: "translate3d(0, 0, 0)",
+          },
+      },
+      },
+      animation: {
+          fadein: 'fade-in 1s ease-in-out 0.25s 1',
+          fadeinleft: 'fade-in-left 1s ease-in-out 0.05s 1',
+        },
+
+    fontFamily: {
+      abc : ["Gupter","EB Garamond"],
+      abc2 : ["Gupter","Roboto Slab"]
+    }
+  }
   },
   plugins: [require("daisyui")],
 };
