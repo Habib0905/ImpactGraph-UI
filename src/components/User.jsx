41f3a769c94300230ae3adb84cc0f8fd1e232992
Habib Hussain <hussainhabib05@yahoo.com>
2024-09-07 20:04:35 +0600

    fix problem of same username

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -7,9 +7,10 @@ const User = () => {
   const [userData, setUserData] = useState({
     username: "",
     password: "",
-    role: "USER", 
+    role: "USER",
   });
   const [users, setUsers] = useState([]);
+  const [userPresent, setUserPresent] = useState(false);
 
   const token = localStorage.getItem("token");
 
@@ -53,7 +54,6 @@ const User = () => {
     setShowPassword(!showPassword);
   };
 
-  
   const deleteUser = (id) => {
     console.log(id);
 
@@ -73,8 +73,6 @@ const User = () => {
       .catch((err) => toast.error(err));
   };
 
-
-
   const handleInputChange = (e) => {
     const { name, value, type, checked } = e.target;
     setUserData((prevData) => ({
@@ -83,16 +81,37 @@ const User = () => {
     }));
   };
 
+  const checkUsernameAndSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .get(`http://localhost:8081/checkUsername/${userData.username}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        console.log(response);
+        if (response.data) {
+          setUserPresent(true);
+        } else {
+          setUserPresent(false);
+          handleSubmit(e);
+        }
+      })
+      .catch((error) => {
+        console.error("There was an error checking the username!", error);
+      });
+  };
+
   return (
     <div>
       <div className="hero min-h-screen relative bg-white flex flex-col justify-center items-center">
-
         <p className="absolute top-10 text-center text-4xl text-pink-900 p-5 mb-14 mx-auto font-abc font-bold">
-          Add Users And Admins
+          Add Users and Admins
         </p>
 
-
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={checkUsernameAndSubmit}>
           <div className="relative mt-40 mb-10 bg-white border-pink-900 border-2 bg-opacity-90 rounded shadow-2xl shadow-white w-[700px] h-auto p-10 mx-auto flex flex-col justify-center items-center">
             <label
               htmlFor="username"
@@ -151,7 +170,11 @@ const User = () => {
               />
               If Admin, Click here
             </label>
-
+            {userPresent && (
+              <div className="text-red-500 mt-7">
+                Username already exists. Please choose a different username!
+              </div>
+            )}
             <button
               className="btn btn-wide bg-white shadow-md border-pink-900 border-2 shadow-pink-900 text-pink-900 mt-9 hover:bg-pink-900 hover:text-white"
               type="submit"
@@ -159,47 +182,47 @@ const User = () => {
               Add User
             </button>
           </div>
-       </form>
+        </form>
 
-   
-      <div className="relative mt-10 z-10 mb-10 bg-white border-pink-900 border-2 bg-opacity-90 rounded shadow-2xl shadow-white w-[700px] h-auto p-10 mx-auto flex flex-col justify-center items-center"> 
-         <h2 className="text-3xl text-center text-pink-900 font-bold mb-5">
-          User Details
-        </h2>
-        <table className="min-w-full bg-white border-collapse border border-pink-900">
-          <thead>
-            <tr>
-              <th className="px-6 py-3 border-b-2 border-pink-900 bg-gray-200 text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
-                Username
-              </th>
-              <th className="px-6 py-3 border-b-2 border-pink-900 bg-gray-200 text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
-                Role
-              </th>
-              <th className="px-6 py-3 border-b-2 border-pink-900 bg-gray-200 text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
-                
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user) => (
-              <tr key={user.id}>
-                <td className="px-6 py-4 border-b border-pink-900 text-sm leading-5 text-gray-800">
-                  {user.username}
-                </td>
-                <td className="px-6 py-4 border-b border-pink-900 text-sm leading-5 text-gray-800">
-                  {user.role}
-                </td>
-                <td className="px-6 py-4 border-b border-pink-900 text-sm leading-5 text-gray-800">
-                 <button className="bg-pink-900 p-3 text-white rounded-md hover:bg-black" onClick={()=>deleteUser(user.id)}>Delete User</button>
-                </td>
-                
+        <div className="relative mt-10 z-10 mb-10 bg-white border-pink-900 border-2 bg-opacity-90 rounded shadow-2xl shadow-white w-[700px] h-auto p-10 mx-auto flex flex-col justify-center items-center">
+          <h2 className="text-3xl text-center text-pink-900 font-bold mb-5">
+            User Details
+          </h2>
+          <table className="min-w-full bg-white border-collapse border border-pink-900">
+            <thead>
+              <tr>
+                <th className="px-6 py-3 border-b-2 border-pink-900 bg-gray-200 text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
+                  Username
+                </th>
+                <th className="px-6 py-3 border-b-2 border-pink-900 bg-gray-200 text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
+                  Role
+                </th>
+                <th className="px-6 py-3 border-b-2 border-pink-900 bg-gray-200 text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider"></th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {users.map((user) => (
+                <tr key={user.id}>
+                  <td className="px-6 py-4 border-b border-pink-900 text-sm leading-5 text-gray-800">
+                    {user.username}
+                  </td>
+                  <td className="px-6 py-4 border-b border-pink-900 text-sm leading-5 text-gray-800">
+                    {user.role}
+                  </td>
+                  <td className="px-6 py-4 border-b border-pink-900 text-sm leading-5 text-gray-800">
+                    <button
+                      className="bg-pink-900 p-3 text-white rounded-md hover:bg-black"
+                      onClick={() => deleteUser(user.id)}
+                    >
+                      Delete User
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-      </div>
-     
     </div>
   );
 };
